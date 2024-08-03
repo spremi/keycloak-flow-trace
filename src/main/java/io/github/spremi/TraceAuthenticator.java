eb8d58b9f7ae6b79dfa38e103fafd0319bf509fb
Sanjeev Premi <spremi@ymail.com>
2024-08-03 21:18:51 +0530

    feat: auth: Implement minimal authenticator
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/main/java/io/github/spremi/TraceAuthenticator.java
+++ src/main/java/io/github/spremi/TraceAuthenticator.java
@@ -8,5 +8,49 @@
 
 package io.github.spremi;
 
-public class TraceAuthenticator {
+import org.jboss.logging.Logger;
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+
+import static io.github.spremi.TraceConstants.LOG_PREFIX;
+
+public class TraceAuthenticator implements Authenticator {
+    private static final Logger logger = Logger.getLogger(TraceAuthenticator.class);
+
+    @Override
+    public void authenticate(AuthenticationFlowContext context) {
+        logger.debug(LOG_PREFIX + "authenticate()");
+    }
+
+    @Override
+    public void action(AuthenticationFlowContext context) {
+        logger.debug(LOG_PREFIX + "action()");
+    }
+
+    @Override
+    public boolean requiresUser() {
+        logger.debug(LOG_PREFIX + "requiresUser()");
+
+        return false;
+    }
+
+    @Override
+    public boolean configuredFor(KeycloakSession session, RealmModel realmModel, UserModel userModel) {
+        logger.debug(LOG_PREFIX + "configuredFor()");
+
+        return false;
+    }
+
+    @Override
+    public void setRequiredActions(KeycloakSession session, RealmModel realmModel, UserModel userModel) {
+        logger.debug(LOG_PREFIX + "setRequiredActions()");
+    }
+
+    @Override
+    public void close() {
+        logger.debug(LOG_PREFIX + "close()");
+    }
 }
